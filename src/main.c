60f4225543bfde2620f7b370db0a73503d4fb158
sporestudio <jrodcas@proton.me>
2025-06-15 17:18:36 +0200

    feat: Add main file
    Implement data structures to store the size of memory allocated.
    Implement basic logic to allocate data in the heap via sbrk.

---
+++ src/main.c
@@ -0,0 +1,67 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+
+struct header_t {
+	size_t size;
+	unsigned is_free;
+	struct header_t *next;
+};
+
+typedef char ALIGN[16];
+
+union header {
+	struct {
+		size_t size;
+		unsigned is_free;
+		union header *next;
+	} s;
+	ALIGN stub;
+};
+typedef union header header_t;
+
+header_t *head, *tail;
+pthread_mutex_t global_malloc_lock;
+
+
+void *malloc(size_t size)
+{
+    size_t total_size;
+    void *block;
+    header_t *header;
+    
+    if (!size) return NULL;
+  
+    pthread_mutex_lock(&global_malloc_lock);
+    header = get_free_block(size);
+
+    if (header) {
+        header->s.is_free = 0;
+        pthread_mutex_lock(&global_malloc_lock);
+        return (void*)(header + 1);
+    }
+
+    total_size = sizeof(header_t) + size;
+    block = sbrk(total_size);
+
+    if (block == (void*)-1) {
+        pthread_mutex_unlock(&global_malloc_lock);
+        return NULL;
+    }
+
+    header = block;
+    header->s.size = size;
+    header->s.is_free = 0;
+    header->s.next = NULL;
+
+    if (!head) head = header;
+    if (tail) tail->s.next = header;
+
+    tail = header;
+    pthread_mutex_unlock(&global_malloc_lock);
+    
+    return (void*)(header + 1);
+}
+
